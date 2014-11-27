467e72c3bd6a990e715bb5f3aef07c39692e6e8e
dk77.yun <dk77.yun@samsung.com>
2014-11-27 10:53:22 +0900

    update script
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -9,21 +9,11 @@ sudo apt-get install -y ctags
 sudo apt-get install -y cscope
 sudo apt-get install -y gcc
 sudo apt-get install -y build-essential
-#sudo apt-get install -y flex bison gperf libsdl-dev libesd0-dev 
-#sudo apt-get install -y libwxgtk2.6-dev build-essential zip curl libncurses5-dev 
-sudo apt-get install -y zlib1g-dev 
+sudo apt-get install -y zlib1g-dev
 sudo apt-get install -y valgrind
 sudo apt-get install -y git-core gnupg
 sudo apt-get install -y git
 sudo apt-get install -y curl
 sudo apt-get install -y vim
-
-#sudo apt-get install -y aptitude
-#sudo aptitude install build-essential linux-headers-$(uname -r)
-#aptitude update
-
-#sudo apt-get install -y compiz
-#aptitude install compizconfig-settings-manager
-#sudo apt-get install -y emerald 
-
+#sudo apt-get install nvidia-current
 
