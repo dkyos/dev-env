034f3d9efe649134221c0a226bd0f7ffffad9f2f
dk77.yun <dk77.yun@samsung.com>
2018-08-17 17:21:33 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

---
+++ bin/dk_proxy.sh
@@ -0,0 +1,14 @@
+
+# my proxy
+#export http_proxy=http://172.21.80.180:3128
+#export https_proxy=http://172.21.80.180:3128
+#export HTTP_PROXY=http://172.21.80.180:3128
+#export HTTPS_PROXY=http://172.21.80.180:3128
+
+export all_proxy=socks://172.21.101.204:3128/
+export ftp_proxy=ftp://172.21.101.204:3128/
+export http_proxy=http://172.21.101.204:3128/
+export https_proxy=https://172.21.101.204:3128/
+export no_proxy=localhost,127.0.0.0/8,::1
+export socks_proxy=socks://172.21.101.204:3128/
+
