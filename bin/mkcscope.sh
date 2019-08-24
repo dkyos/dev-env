acb3c63f60ee9c51f4843513fe8df489da16d33f
dk77.yun <dk77.yun@samsung.com>
2019-08-24 19:34:44 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- bin/mkcscope.sh
+++ bin/mkcscope.sh
@@ -4,4 +4,7 @@ rm -rf cscope.files cscope.files
 
 find `pwd` \( -name '*.java' -o -name '*.groovy' -o -name '*.c' -o -name '*.py' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
 
+cscope -Rb
 cscope -i cscope.files
+#cscope -i cscope.files
+
